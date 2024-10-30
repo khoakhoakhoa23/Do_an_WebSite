5e36f13a9e7093eb43d701858e2a94ed825c113e
khoakhoakhoa23 <tanhkhoa06@gmail.com>
2024-10-30 10:23:14 +0700

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>website ban do dien tu</title>
+</head>
+<body>
+	<div class="wrapper">
+		<?php
+			include("admincp/config/config.php");
+			include("pages/header.php");
+			include("pages/menu.php");
+			include("pages/main.php");
+			include("pages/footer.php");
+		?>
+	</div>		
+	
+</body>
+</html>
